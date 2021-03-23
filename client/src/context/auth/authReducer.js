ffe15fc6ba842e1a7430323cb617755ef8e17471
ashwaniYDV <ashyadavash@gmail.com>
2021-03-23 18:03:06 +0530

    updates

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -10,6 +10,7 @@ import {
     CLEAR_ERRORS,
 } from "../types";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
     switch (action.type) {
         case SET_LOADING:
